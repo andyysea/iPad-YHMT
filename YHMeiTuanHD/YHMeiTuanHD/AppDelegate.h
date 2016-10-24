07740d74978bd7eee135b133963b2a89ae584b46
YangYingHai <601503988@qq.com>
2016-10-24 22:11:59 +0800

    初始化项目

---
+++ YHMeiTuanHD/YHMeiTuanHD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YHMeiTuanHD
+//
+//  Created by 杨应海 on 16/10/24.
+//  Copyright © 2016年 YYH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YHMeiTuanHD/YHMeiTuanHD/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YHMeiTuanHD
+//
+//  Created by 杨应海 on 16/10/24.
+//  Copyright © 2016年 YYH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
