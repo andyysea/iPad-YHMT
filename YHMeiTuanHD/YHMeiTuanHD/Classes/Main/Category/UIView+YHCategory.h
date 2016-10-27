296000813ced8bd266061c9711dce7a0ce1f4af5
YangYingHai <601503988@qq.com>
2016-10-27 13:34:43 +0800

    添加视图分类

---
+++ YHMeiTuanHD/YHMeiTuanHD/Classes/Main/Category/UIView+YHCategory.h
@@ -0,0 +1,22 @@
+//
+//  UIView+YHCategory.h
+//  YHMeiTuanHD
+//
+//  Created by 杨应海 on 16/10/27.
+//  Copyright © 2016年 YYH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (YHCategory)
+
+@property (nonatomic, assign) CGFloat x;
+@property (nonatomic, assign) CGFloat y;
+@property (nonatomic, assign) CGFloat width;
+@property (nonatomic, assign) CGFloat height;
+@property (nonatomic, assign) CGFloat centerX;
+@property (nonatomic, assign) CGFloat centerY;
+@property (nonatomic, assign) CGSize size;
+@property (nonatomic, assign) CGPoint origin;
+
+@end
