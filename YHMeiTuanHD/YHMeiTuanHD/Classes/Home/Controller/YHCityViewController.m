896ddd87d96d870eae356c5de8502fda736b9e55
YangYingHai <601503988@qq.com>
2016-10-30 22:32:46 +0800

    搜索按钮的代理方法设置覆盖按钮

--- YHMeiTuanHD/YHMeiTuanHD/Classes/Home/Controller/YHCityViewController.m
+++ YHMeiTuanHD/YHMeiTuanHD/Classes/Home/Controller/YHCityViewController.m
@@ -9,9 +9,11 @@
 #import "YHCityViewController.h"
 #import "YHCityGroupModel.h"
 
-@interface YHCityViewController () <UITableViewDataSource>
+@interface YHCityViewController () <UITableViewDataSource, UISearchBarDelegate>
 
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
+/** 遮盖按钮 */
+@property (weak, nonatomic) IBOutlet UIButton *coverButton;
 
 /** cityGroupPlist的模型数据 */
 @property (nonatomic, strong) NSArray *cityGroupArray;
@@ -29,7 +31,50 @@
     // 设置表格右侧索引标题颜色 下面两种方法均可
 //    self.tableView.tintColor = YHColor(21, 188, 173, 1.0);
     self.tableView.sectionIndexColor = YHColor(21, 188, 173, 1.0);
-} 
+}
+
+#pragma mark - UISearchBarDelegate
+/** 
+    开始编辑
+ */
+- (void)searchBarTextDidBeginEditing:(UISearchBar *)searchBar {
+    // 1> 隐藏导航栏
+    self.navigationController.navigationBarHidden = YES;
+    // 2> 修改搜索框颜色
+    searchBar.backgroundImage = [UIImage imageNamed:@"bg_login_textfield_hl"];
+    // 3> 取消按钮
+    [searchBar setShowsCancelButton:YES animated:YES];
+    // 4> 遮盖按钮
+    self.coverButton.alpha = 0.3;
+}
+
+/** 
+    结束编辑
+ */
+- (void)searchBarTextDidEndEditing:(UISearchBar *)searchBar {
+    
+    // 1> 隐藏导航栏
+    self.navigationController.navigationBarHidden = NO;
+    // 2> 修改搜索框颜色
+    searchBar.backgroundImage = [UIImage imageNamed:@"bg_login_textfield"];
+    // 3> 取消按钮
+    [searchBar setShowsCancelButton:NO animated:YES];
+    // 4> 遮盖按钮
+    self.coverButton.alpha = 0;
+
+}
+
+/** 
+    点击搜索框取消按钮
+ */
+- (void)searchBarCancelButtonClicked:(UISearchBar *)searchBar {
+    [searchBar resignFirstResponder];
+}
+
+#pragma mark 遮盖按钮点击方法
+- (IBAction)coverButtonClick:(id)sender {
+    [self.view endEditing:YES];
+}
 
 #pragma mark - UITableViewDataSource
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
